f9134e57ce5ee2404d4deb0e28d3c9f9ab4b5459
zeqipe <zeqipe@vk.com>
2024-02-12 03:36:35 +0300

    create class App

---
+++ core/application.py
@@ -0,0 +1,12 @@
+class App:
+
+    def __init__(self):
+        self.container = self.load_container()
+
+    @staticmethod
+    def load_container():
+        pass
+
+    @staticmethod
+    def run_app():
+        pass
--- entry_point.py
+++ entry_point.py
@@ -1,1 +1,6 @@
+from core.application import App
 
+# The entry point to the application
+if __name__ == "__main__":
+    contact_book = App()
+    contact_book.run_app()
